78d3ee5716ca05bacd157c8aa58168480a0f820f
Alexsander Vieira Junior <alexsandervieirajunior@hotmail.com>
2019-03-14 23:10:42 -0300

    Update
    Bug fix.

--- ProcessDataCep.py
+++ ProcessDataCep.py
@@ -5,11 +5,17 @@ import json
 class ProcessDataCep():
         driver = webdriver.Chrome(executable_path=r"chromedriver.exe")
         driver.get("http://www.buscacep.correios.com.br/sistemas/buscacep/buscaFaixaCep.cfm")
+        dicDad = {}
 
-        for i in range(0,1):
+        for i in range(0,2):
                 driver.find_element_by_xpath('//*[@id="Geral"]/div/div/span[2]/label/select').click()
-                state = driver.find_element_by_xpath('//*[@id="Geral"]/div/div/span[2]/label/select/option[25]')
-                driver.find_element_by_xpath('//*[@id="Geral"]/div/div/span[2]/label/select/option[25]').click()
+
+                if i == 0:
+                        state = 'SC'
+                        driver.find_element_by_xpath('//*[@id="Geral"]/div/div/span[2]/label/select/option[25]').click()
+                else:
+                        state = 'SP'
+                        driver.find_element_by_xpath('//*[@id="Geral"]/div/div/span[2]/label/select/option[27]').click()
                 driver.find_element_by_xpath('//*[@id="Geral"]/div/div/div[4]/input').click()
 
 
@@ -18,31 +24,36 @@ class ProcessDataCep():
                 columns = []
                 values = {}
                 valuesTable = []
-                while len(valuesTable) <= 100:
-                        elem = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/table[2]").is_displayed()
+                count = 0
+                while valuesTable.__len__() != 100:
 
-                        if elem:
+                        if count == 0:
                                 table = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/table[2]")
                         else:
                                 table = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/table")
                         columns = (table.find_elements_by_tag_name('td'))
 
-                        for index in range(0, len(columns), 4):
+                        count += 1
+
+                        for index in range(0, columns.__len__(), 4):
                                 values.update({"Location":columns[index].text})
                                 values.update({"CepsRange":columns[index + 1].text})
                                 valuesTable.append(values)
                                 values = {}
-                                if len(valuesTable) == 100:
+                                if valuesTable.__len__() >= 100:
                                         break
                         columns = []
-                        table = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/div[5]/a").click()
+                        driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/div[5]/a").click()
 
 
 
-                dicDad = {'1':valuesTable}
-                file = open('result.json', 'w')
-                json.dump(dicDad, file)
-                driver.close();
+                driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/div[6]/a").click()
+
+                dicDad.update({state:valuesTable})
+        file = open('result.json', 'w')
+        json.dump(dicDad, file)
+        driver.close();
+
 
 if __name__ == '__main__':
     unittest.main
